bf199379566117546151244800a4efc731fdb3c7
André Lashley <andre.lashley@gmail.com>
2022-10-20 23:21:34 -0700

    Add delegates to allow consumers of the order processor to extend the order process

--- src/03/Start_Here/WarehouseManagementSystem/WarehouseManagementSystem.Business/OrderProcessor.cs
+++ src/03/Start_Here/WarehouseManagementSystem/WarehouseManagementSystem.Business/OrderProcessor.cs
@@ -4,17 +4,26 @@ namespace WarehouseManagementSystem.Business
 {
     public class OrderProcessor
     {
+        public delegate void OrderInitialized();
+        public delegate void ProcessCompleted();
+
+        public OrderInitialized OnOrderInitialized { get; set; }
+
         private void Initialize(Order order)
         {
+            ArgumentNullException.ThrowIfNull(order);
+
+            OnOrderInitialized?.Invoke();
         }
 
-        public void Process(Order order)
+        public void Process(Order order, ProcessCompleted onComplete = default)
         {
             // Run some code..
 
             Initialize(order);
 
             // How do I produce a shipping label?
+            onComplete?.Invoke();
         }
     }
 }
--- src/03/Start_Here/WarehouseManagementSystem/WarehouseManagementSystem/Program.cs
+++ src/03/Start_Here/WarehouseManagementSystem/WarehouseManagementSystem/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using WarehouseManagementSystem.Business;
+using WarehouseManagementSystem.Domain;
+
+var order = new Order
+{
+    LineItems = new[] {
+        new Item { Name = "PS2", Price = 50 },
+        new Item { Name = "PS4", Price = 60 },
+        new Item { Name = "PS5", Price = 70 },
+        new Item { Name = "PS6", Price = 80 },
+        }
+};
+
+var processor = new OrderProcessor
+{
+    OnOrderInitialized = SendMessageToWarehouse
+};
+
+processor.Process(order, SendConfirmationEmail);
+
+void SendMessageToWarehouse()
+{
+    Console.WriteLine("Please pack the order");
+}
+
+void SendConfirmationEmail()
+{
+    Console.WriteLine("Order confirmation Email");
+}
