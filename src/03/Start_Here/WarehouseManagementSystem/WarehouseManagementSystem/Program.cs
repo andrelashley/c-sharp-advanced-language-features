899a2b05e6efed10598ac26abc9d6612e7e7a246
André Lashley <andre.lashley@gmail.com>
2022-10-22 19:03:27 -0700

    Add multicast/chained delegates

--- src/03/Start_Here/WarehouseManagementSystem/WarehouseManagementSystem/Program.cs
+++ src/03/Start_Here/WarehouseManagementSystem/WarehouseManagementSystem/Program.cs
@@ -16,7 +16,20 @@ var processor = new OrderProcessor
     OnOrderInitialized = SendMessageToWarehouse
 };
 
-processor.Process(order, SendConfirmationEmail);
+OrderProcessor.ProcessCompleted chain = One;
+chain += Two;
+chain += Three;
+
+processor.Process(order, chain);
+
+chain -= Two;
+processor.Process(order, chain);
+
+
+void One(Order order) => Console.WriteLine("One");
+void Two(Order order) => Console.WriteLine("Two");
+void Three(Order order) => Console.WriteLine("Three");
+
 
 bool SendMessageToWarehouse(Order order)
 {
